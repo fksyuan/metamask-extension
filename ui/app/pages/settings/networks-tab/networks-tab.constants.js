fa51655547ec99643210c55992ecbad390da3bb8
xuwen <xuwen@juzix.net>
2020-10-21 12:03:28 +0800

    update alaya network rpcTarget to domain name

--- ui/app/pages/settings/networks-tab/networks-tab.constants.js
+++ ui/app/pages/settings/networks-tab/networks-tab.constants.js
@@ -3,7 +3,7 @@ const defaultNetworksData = [
     labelKey: 'alaya',
     iconColor: '#F6C343',
     providerType: 'alaya',
-    rpcUrl: 'http://10.1.1.50:6790',
+    rpcUrl: 'http://samurai.alaya.network',
     chainId: '201018',
     ticker: 'ATP',
     blockExplorerUrl: 'https://scan.alaya.network.io',
